60a6ef6908678bfbbd7ad278d8673e826996219d
wxlf512 <wxlf512@gmail.com>
2022-03-19 12:00:25 +0300

    Added http request

--- app/src/main/java/dev/wxlf/cft/MainActivity.java
+++ app/src/main/java/dev/wxlf/cft/MainActivity.java
@@ -3,12 +3,56 @@ package dev.wxlf.cft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTxt = findViewById(R.id.txt);
+
+        OkHttpClient client = new OkHttpClient();
+        Request req = new Request.Builder().get().url("https://www.cbr-xml-daily.ru/daily_json.js").build();
+        Call call = client.newCall(req);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast toast = new Toast(getApplicationContext());
+                toast.setText("Получить не удалось :(");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseStr = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTxt.setText(responseStr);
+                    }
+                });
+
+            }
+        });
     }
 }
