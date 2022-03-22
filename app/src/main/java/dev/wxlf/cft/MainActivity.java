41ec0cc0efaef14e7dce9b0d2bf71f5b20341371
wxlf512 <wxlf512@gmail.com>
2022-03-22 23:57:43 +0300

    Added conversion from rubles

--- app/src/main/java/dev/wxlf/cft/MainActivity.java
+++ app/src/main/java/dev/wxlf/cft/MainActivity.java
@@ -1,8 +1,12 @@
 package dev.wxlf.cft;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,26 +26,70 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int selectedPos;
-    ArrayList<Currency> currencies = new ArrayList<>();
+    private String selectedPos;
+    final private ArrayList<Currency> currencies = new ArrayList<>();
     final private String[] curList = {"AUD", "AZN", "GBP", "AMD", "BYN", "BGN", "BRL", "HUF", "HKD",
             "DKK", "USD", "EUR", "INR", "KZT", "CAD", "KGS", "CNY", "MDL", "NOK", "PLN", "RON",
             "XDR", "SGD", "TJS", "TRY", "TMT", "UZS", "UAH", "CZK", "SEK", "CHF", "ZAR", "KRW", "JPY"};
     private CurrencyAdapter adapter;
+    private JSONObject jsonCurrencies;
+    private EditText rubText;
+    private TextView valText, rvalText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rubText = findViewById(R.id.rub);
+        valText = findViewById(R.id.val);
+        rvalText = findViewById(R.id.rval);
+
+        rubText.setOnKeyListener(new View.OnKeyListener()
+                                  {
+                                      @SuppressLint({"SetTextI18n", "DefaultLocale"})
+                                      public boolean onKey(View v, int keyCode, KeyEvent event)
+                                      {
+                                          if(event.getAction() == KeyEvent.ACTION_UP)
+                                          {
+                                              if (rubText.getText().length() != 0)
+                                                  try {
+                                                      double cur = jsonCurrencies.getJSONObject(selectedPos).getDouble("Value");
+                                                      double rub = Double.parseDouble(rubText.getText().toString());
+                                                      valText.setText(String.format("%.2f", rub / cur));
+                                                  } catch (JSONException e) {
+                                                      e.printStackTrace();
+                                                  }
+                                              else
+                                                  valText.setText("0.00");
+                                              rvalText.setText("RUB/" + selectedPos);
+                                              valText.setHint(selectedPos);
+                                              return true;
+                                          }
+                                          return false;
+                                      }
+                                  }
+        );
         getInfoFromCB();
-        //setTestData();
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         CurrencyAdapter.OnCurrencyClickListener currencyClickListener = new CurrencyAdapter.OnCurrencyClickListener() {
+            @SuppressLint({"DefaultLocale", "SetTextI18n"})
             @Override
             public void onCurrencyClick(Currency currency, int position) {
-                Toast.makeText(getApplicationContext(), "Выбран " + currency.getName(), Toast.LENGTH_SHORT).show();
-                selectedPos = position;
+                //Toast.makeText(getApplicationContext(), "Выбран " + currency.getName(), Toast.LENGTH_SHORT).show();
+                selectedPos = currency.getCharCode();
+                if (rubText.getText().length() != 0)
+                    try {
+                        double cur = jsonCurrencies.getJSONObject(selectedPos).getDouble("Value");
+                        double rub = Double.parseDouble(rubText.getText().toString());
+                        valText.setText(String.format("%.2f", rub / cur));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                else
+                    valText.setText("0.00");
+                rvalText.setText("RUB/" + selectedPos);
+                valText.setHint(selectedPos);
             }
         };
         adapter = new CurrencyAdapter(this, currencies, currencyClickListener);
@@ -67,10 +115,9 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         try {
                             JSONObject jsonRoot = new JSONObject(responseStr);
-                            JSONObject jsonCurrencies = jsonRoot.getJSONObject("Valute");
+                            jsonCurrencies = jsonRoot.getJSONObject("Valute");
                             for (int i = 0; i < curList.length; i++) {
                                 JSONObject currency = jsonCurrencies.getJSONObject(curList[i]);
-                                Log.i(curList[i], jsonCurrencies.getString(curList[i]));
                                 currencies.add(new Currency(currency.getString("CharCode"),
                                         currency.getString("Name"),
                                         currency.getString("Value")));
@@ -84,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     public void updateList(View view) {
         getInfoFromCB();
         adapter.notifyDataSetChanged();
